094d37e80bd7767723f4b21073dd5451f3e04963
David C. Manuelda <StormByte@gmail.com>
2026-08-15 20:15:18 +0200

    Updated platform support macros

--- lib/public/StormByte/platform.h
+++ lib/public/StormByte/platform.h
@@ -1,16 +1,30 @@
 #pragma once
 
-// Since only Windows and Linux OS is supported...
-#if defined _WIN32 || defined __CYGWIN__
-	#define WINDOWS
+// ---------------------------------------------------------------------------
+// Operating system
+// ---------------------------------------------------------------------------
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #define WINDOWS
+#elif defined(__APPLE__) && defined(__MACH__)
+    #define MACOS
+    #define UNIX
+#elif defined(__linux__)
+    #define LINUX
+    #define UNIX
+#elif defined(__unix__) || defined(__unix)
+    // Other unices (BSD, etc.) if you ever need them
+    #define UNIX
 #else
-	#define LINUX
+    #error "Unsupported operating system"
 #endif
 
+// ---------------------------------------------------------------------------
+// Compiler
+// ---------------------------------------------------------------------------
 #if defined(__clang__)
-#define CLANG
+    #define CLANG
 #elif defined(__GNUC__) || defined(__GNUG__)
-#define GCC
+    #define GCC
 #elif defined(_MSC_VER)
-#define MSVC
+    #define MSVC
 #endif
