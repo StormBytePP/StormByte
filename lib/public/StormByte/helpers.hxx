11d0e691fd3ac901af2278959697e9c8074b6e4f
David C. Manuelda <StormByte@gmail.com>
2026-09-04 19:34:28 +0200

    docs(helpers): document span append_vector; fix move-overload notes

--- lib/public/StormByte/helpers.hxx
+++ lib/public/StormByte/helpers.hxx
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte.
- *
- * StormByte is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte.
+*
+* StormByte is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
 
@@ -28,12 +28,17 @@
 
 /**
  * @namespace StormByte
- * @brief Main namespace for the StormByte library.
- *
- * The `StormByte` namespace serves as the root for all components and utilities in the StormByte library.
- * It provides foundational classes and tools for building robust, thread-safe, and efficient applications.
+ * @brief Root namespace of the StormByte suite.
  */
 namespace StormByte {
+	/**
+	 * @brief Appends a span of convertible elements onto a vector.
+	 * @tparam T Destination element type.
+	 * @tparam U Source element type (`Type::ConvertibleTo<T, U>`).
+	 * @param dest Vector that receives the elements.
+	 * @param src View over the elements to copy.
+	 * @note Reserves once, then uses `append_range` when the library has it.
+	 */
 	template<typename T, typename U>
 	void append_vector(std::vector<T>& dest, std::span<U> src) noexcept requires Type::ConvertibleTo<T, U> {
 		dest.reserve(dest.size() + src.size());
@@ -45,16 +50,10 @@ namespace StormByte {
 	}
 
 	/**
-	 * @brief Appends the contents of one vector to another.
-	 * @tparam T The type of elements in the vectors.
-	 * @param dest The destination vector to which elements will be appended.
-	 * @param src The source vector from which elements will be taken.
-	 * @note This function reserves enough space in the destination vector to accommodate
-	 *       the new elements before appending, to avoid multiple reallocations.
-	 *
-	 * This function appends all elements from the source vector `src` to the end of the
-	 * destination vector `dest`. It supports both lvalue and rvalue source vectors for
-	 * efficient appending.
+	 * @brief Appends a vector by copy.
+	 * @tparam T Element type.
+	 * @param dest Vector that receives the elements.
+	 * @param src Source vector (read-only).
 	 */
 	template<typename T>
 	void append_vector(std::vector<T>& dest, const std::vector<T>& src) noexcept {
@@ -62,16 +61,11 @@ namespace StormByte {
 	}
 
 	/**
-	 * @brief Move the contents of one vector to another.
-	 * @tparam T The type of elements in the vectors.
-	 * @param dest The destination vector to which elements will be appended.
-	 * @param src The source vector from which elements will be taken.
-	 * @note This function reserves enough space in the destination vector to accommodate
-	 *       the new elements before appending, to avoid multiple reallocations.
-	 *
-	 * This function appends all elements from the source vector `src` to the end of the
-	 * destination vector `dest`. It supports both lvalue and rvalue source vectors for
-	 * efficient appending.
+	 * @brief Appends a vector by move.
+	 * @tparam T Element type.
+	 * @param dest Vector that receives the elements.
+	 * @param src Source vector; elements are moved out.
+	 * @note Reserves once, then `append_range(std::move(src))` when available.
 	 */
 	template<typename T>
 	void append_vector(std::vector<T>& dest, std::vector<T>&& src) noexcept {
