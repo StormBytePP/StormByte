6a55f9b56ce302ecd606f1c13050ba7ecf658af3
David C. Manuelda <StormByte@gmail.com>
2026-09-04 19:36:49 +0200

    docs(system): document System namespace; shorten StormByte brief

--- lib/public/StormByte/system.hxx
+++ lib/public/StormByte/system.hxx
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte.
- *
- * StormByte is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte.
+*
+* StormByte is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
 
@@ -27,36 +27,37 @@
 
 /**
  * @namespace StormByte
- * @brief Main namespace for the StormByte library.
- *
- * The `StormByte` namespace serves as the root for all components and utilities in the StormByte library.
- * It provides foundational classes and tools for building robust, thread-safe, and efficient applications.
+ * @brief Root namespace of the StormByte suite.
+ */
+/**
+ * @namespace StormByte::System
+ * @brief Path and timing helpers that hide OS differences.
  */
 namespace StormByte::System {
 	/**
-	 * @brief Safely gets a temporary file name.
-	 * @param prefix The prefix for the temporary file name. Defaults to "TMP".
-	 * @return The full path of the temporary file.
+	 * @brief Builds a unique temporary file path.
+	 * @param prefix File-name prefix. Default `"TMP"`.
+	 * @return Absolute path of the temporary file.
 	 */
 	STORMBYTE_PUBLIC std::filesystem::path TempFileName(const std::string& prefix = "TMP");
 
 	/**
-	 * @brief Gets the current working directory.
-	 * @return The current working directory path.
+	 * @brief Current working directory.
+	 * @return Absolute path of the process cwd.
 	 */
 	STORMBYTE_PUBLIC std::filesystem::path CurrentPath();
 
 	/**
-	 * @brief Gets the directory that contains the running executable.
-	 * @return The directory path of the executable, or "NOPATH" on failure.
+	 * @brief Directory that contains the running executable.
+	 * @return That directory, or the path `"NOPATH"` if it cannot be resolved.
 	 */
 	STORMBYTE_PUBLIC std::filesystem::path ExecutablePath();
 
 	/**
-	 * @brief Sleeps for a specific duration.
-	 * @tparam Rep The representation of the duration.
-	 * @tparam Period The period of the duration.
-	 * @param duration The time to sleep, specified as a `std::chrono::duration`.
+	 * @brief Suspends the calling thread.
+	 * @tparam Rep Duration representation.
+	 * @tparam Period Duration period.
+	 * @param duration Sleep interval.
 	 */
 	template <typename Rep, typename Period>
 	STORMBYTE_PUBLIC void Sleep(const std::chrono::duration<Rep, Period>& duration);
