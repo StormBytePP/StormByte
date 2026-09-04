73bd297772ebe309f0e9a8258eb192b15e51099c
David C. Manuelda <StormByte@gmail.com>
2026-09-04 19:43:05 +0200

    docs(clonable): document concept and clone/move API; shorten namespace brief

--- lib/public/StormByte/clonable.hxx
+++ lib/public/StormByte/clonable.hxx
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte.
- *
- * StormByte is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte.
+*
+* StormByte is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
 
@@ -24,36 +24,36 @@
 
 /**
  * @namespace StormByte
- * @brief Main namespace for the StormByte library.
- *
- * The `StormByte` namespace serves as the root for all components and utilities in the StormByte library.
- * It provides foundational classes and tools for building robust, thread-safe, and efficient applications.
+ * @brief Root namespace of the StormByte suite.
  */
 namespace StormByte {
 	/**
-	 * @brief Concept to check if a type is a valid smart pointer
-	 * @tparam SmartPointer smart pointer type
-	 * @tparam T type
+	 * @brief `true` when @p SmartPointer is `shared_ptr<T>` or `unique_ptr<T>`.
+	 * @tparam SmartPointer Candidate pointer type.
+	 * @tparam T Pointee type.
 	 */
 	template<typename SmartPointer, typename T>
-	concept ValidSmartPointer = 
-		std::same_as<SmartPointer, std::shared_ptr<T>> || 
+	concept ValidSmartPointer =
+		std::same_as<SmartPointer, std::shared_ptr<T>> ||
 		std::same_as<SmartPointer, std::unique_ptr<T>>;
 
 	/**
 	 * @class Clonable
-	 * @brief A class that can be cloned
+	 * @brief Polymorphic clone/move into a smart pointer of type @p T.
+	 * @tparam T Most-derived interface stored in the pointer.
+	 * @tparam SmartPointer `std::shared_ptr<T>` (default) or `std::unique_ptr<T>`.
 	 */
 	template<class T, typename SmartPointer = std::shared_ptr<T>>
 	requires ValidSmartPointer<SmartPointer, T> class Clonable {
 		public:
-			using PointerType	= SmartPointer;	///< Pointer type
+			using PointerType	= SmartPointer;	///< `SmartPointer` alias.
+
 			/**
-			 * Makes a pointer
-			 * @tparam Target The type of the object to create a pointer for (can be `T` or a derived type).
-			 * @tparam Args The constructor arguments for `Target`.
-			 * @param args Arguments to forward to `Target`'s constructor.
-			 * @return A smart pointer of type `PointerType` managing the newly created `Target`.
+			 * @brief Allocates @p Target and returns it as `PointerType`.
+			 * @tparam Target Concrete type (`T` or derived).
+			 * @tparam Args Constructor argument types.
+			 * @param args Forwarded to `Target`.
+			 * @return `shared_ptr` or `unique_ptr` according to `PointerType`.
 			 */
 			template<class Target, typename... Args>
 			static PointerType MakePointer(Args&&... args) {
@@ -65,46 +65,46 @@ namespace StormByte {
 					static_assert(false, "Unsupported smart pointer type");
 				}
 			}
-			
+
 			/**
-			 * Constructor
+			 * @brief Default constructor.
 			 */
 			constexpr Clonable() 											= default;
 
 			/**
-			 * Copy constructor
+			 * @brief Copy constructor.
 			 */
 			constexpr Clonable(const Clonable&) 							= default;
 
 			/**
-			 * Move constructor
+			 * @brief Move constructor.
 			 */
 			constexpr Clonable(Clonable&&) noexcept							= default;
 
 			/**
-			 * Assignment operator
+			 * @brief Copy assignment.
 			 */
 			constexpr Clonable& operator=(const Clonable&) 					= default;
 
 			/**
-			 * Move assignment operator
+			 * @brief Move assignment.
 			 */
 			constexpr Clonable& operator=(Clonable&&) noexcept				= default;
 
 			/**
-			 * Destructor
+			 * @brief Destructor.
 			 */
 			virtual constexpr ~Clonable() noexcept 							= default;
 
 			/**
-			 * Clones the object
-			 * @return cloned object
+			 * @brief Deep copy into a new `PointerType`.
+			 * @return Owning pointer to the clone.
 			 */
 			virtual PointerType Clone() const 								= 0;
 
 			/**
-			 * Moves the object
-			 * @return moved object
+			 * @brief Moves this object into a new `PointerType`.
+			 * @return Owning pointer to the moved instance.
 			 */
 			virtual PointerType Move() 										= 0;
 	};
